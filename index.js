bf275ccc2f48de2b84322978f94082e41bcaeb15
Aaron Green <green.aaron2298@gmail.com>
2022-08-05 12:11:02 -0400

    updated module 4 challenge objects project

--- index.js
+++ index.js
@@ -152,7 +152,8 @@ Use the getLastReview function below to do the following:
 
 
 function getLastReview(array) {
-  return reviews[7].name + " gave the restaurant a " + reviews[7].rating + " star review, and their feedback was:" + reviews[7].feedback
+  return array[array.length - 1].name + " gave the restaurant a " + array[array.length - 1].rating + " star review, and their feedback was: " + array[array.length - 1].feedback
+}
 console.log('task 7', getLastReview(reviews));
 
 
